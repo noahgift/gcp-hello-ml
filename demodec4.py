7bf3a9dd482aacea41b01a01d7bc533bcea2827d
Noah Gift <noah.gift@gmail.com>
2019-12-04 10:26:17 -0800

    Create demodec4.py

---
+++ demodec4.py
@@ -0,0 +1,55 @@
+from flask import Flask
+from flask import jsonify
+from time import gmtime, strftime
+import pandas as pd
+import wikipedia
+from textblob import TextBlob
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World December 4th!'
+
+@app.route('/name/<value>')
+def name(value):
+    print(f"This value was passed into a URL: /name/{value}")
+    if value == "bear":
+        val = {"mammal": value}
+    else:
+        val = {"unknown": value}
+    return jsonify(val)
+
+@app.route('/time')
+def time():
+    my_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    print(f"This was the time I returned")
+    my_time_dict = {"time": my_time}
+    return jsonify(my_time_dict)
+
+@app.route('/html')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <p>Hello:  It is December 4th, 2019</p>
+    <p><b>World</b></p>
+    """
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+    result = wikipedia.summary(company, sentences=10)
+    res = TextBlob(result)
+    val = {"company": company, 
+        "summary": result,
+        "polarity": res.sentiment.polarity}
+    return jsonify(val)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
