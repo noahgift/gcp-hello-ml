0acb43b5484f7d56193a16e23b3b01c997f62385
Lucas Liu <lucas.liu@duke.edu>
2021-01-24 04:24:32 +0000

    updated google nlp calls

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
-import logging
-
 from flask import Flask
-app = Flask(__name__)
+from flask import jsonify
+import pandas as pd
+import wikipedia
 
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello I like to make AI Apps'
 
-@app.route('/newroute/<name>')
-def newroute(name):
-    """parameter"""
-    return "this was passed in: %s" % name
+@app.route('/name/<value>')
+def name(value):
+    val = {"value": value}
+    return jsonify(val)
 
-
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
+@app.route('/html')
+def html():
+    """Returns some custom HTML"""
     return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
+    <title>This is a Hello World World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
+
+@app.route('/wikipedia/<company>')
+def wikipedia_route(company):
+
+    # Imports the Google Cloud client library
+    from google.cloud import language
+    result = wikipedia.summary(company, sentences=10)
+
+    client = language.LanguageServiceClient()
+    document = language.Document(
+        content=result,
+        type_=language.Document.Type.PLAIN_TEXT)
+    encoding_type = language.EncodingType.UTF8
+    entities = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type}).entities
+    return str(entities)
 
 
 if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
